78b3c40b7caf9fc5496c2633a36de8aa076e1eab
sxopedesigner <developer6.sxope@gmail.com>
2019-08-17 12:01:35 +0530

    Checking URL exists or not using curl

--- index.php
+++ index.php
@@ -14,4 +14,28 @@ foreach($url->find('a') as $e)
 echo '<h1>Images</h1><hr>';
 foreach($url->find('img') as $e)
     echo $e->src . '<br>';
+
+echo '<h1>Check if URL exists or not</h1><hr>';
+
+// Initializing new session
+$ch = curl_init("http://cypherox.com/");
+
+// Request method is set
+curl_setopt($ch, CURLOPT_NOBODY, true);
+
+// Executing cURL session
+curl_exec($ch);
+
+// Getting information about HTTP Code
+$retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+// Testing for 404 Error
+if($retcode != 200) {
+    echo "Specified URL does not exist";
+}
+else {
+    echo "Specified URL exists";
+}
+
+curl_close($ch);
 ?>
