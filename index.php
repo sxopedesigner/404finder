79a22c8786f7d1beed430ff218742ea71da1ad2e
sxopedeveloper <developer11.sxope@gmail.com>
2019-08-19 11:04:42 +0530

    URL Validation

--- index.php
+++ index.php
@@ -1,41 +1,28 @@
-<?php
-// Include the library
-include('simplehtmldom/simple_html_dom.php');
- 
-// Retrieve the DOM from a given URL
-$url = file_get_html('https://cypherox.com/');
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Check URL</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<form action="submit.php" method="POST">
+			<label class="label">INPUT URL HERE</label>
+			<input class="form-control form-control-sm" type="url" name="url" id="url" required>	
 
-// Find all "A" tags and print their HREFs
-echo '<h1>Links</h1><hr>';
-foreach($url->find('a') as $e) 
-    echo $e->href . '<br>';
-    
-// Retrieve all images and print their SRCs
-echo '<h1>Images</h1><hr>';
-foreach($url->find('img') as $e)
-    echo $e->src . '<br>';
+			<button type="submit" class="btn btn-sm btn-success">Submit</button>
+			<button type="reset" class="btn btn-sm btn-danger">Clear</button>
+			</form>
+		</div>
+	</div>
 
-echo '<h1>Check if URL exists or not</h1><hr>';
-
-// Initializing new session
-$ch = curl_init("http://cypherox.com/");
-
-// Request method is set
-curl_setopt($ch, CURLOPT_NOBODY, true);
-
-// Executing cURL session
-curl_exec($ch);
-
-// Getting information about HTTP Code
-$retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-// Testing for 404 Error
-if($retcode != 200) {
-    echo "Specified URL does not exist";
-}
-else {
-    echo "Specified URL exists";
-}
-
-curl_close($ch);
-?>
+</body>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="  crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/inputmask/4.0.8/inputmask/bindings/inputmask.binding.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		   
+	});
+</script>
+</html>
